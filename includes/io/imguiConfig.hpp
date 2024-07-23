5f36cc79b32fb38dd1074059cf3b31ca0cec8934
razor7877 <65608588+razor7877@users.noreply.github.com>
2024-07-23 16:11:09 +0200

    Added ImGui config for implicit ImVec to glm vectors conversions

---
+++ includes/io/imguiConfig.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+// We define implicit operators so that ImVec2 and ImVec4 can be directly retrieved and converted in corresponding glm vectors
+
+#define IM_VEC2_CLASS_EXTRA                                                     \
+        constexpr ImVec2(const glm::vec2& f) : x(f.x), y(f.y) {}                   \
+        operator glm::vec2() const { return glm::vec2(x, y); }
+
+#define IM_VEC4_CLASS_EXTRA                                                     \
+        constexpr ImVec4(const glm::vec4& f) : x(f.x), y(f.y), z(f.z), w(f.w) {}   \
+        operator glm::vec4() const { return glm::vec4(x, y, z, w); }
