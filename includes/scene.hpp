246787bd13dab649560086f147d69239e1e66c1a
razor7877 <65608588+razor7877@users.noreply.github.com>
2024-08-29 22:24:17 +0200

    Added Scene class to hold scene data for each game state

---
+++ includes/scene.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <map>
+#include <vector>
+
+#include "shader.hpp"
+#include "entity.hpp"
+#include "components/meshComponent.hpp"
+#include "physics/frustum.hpp"
+#include "components/lights/directionalLightComponent.hpp"
+
+struct SortedSceneData
+{
+	// Opaque entities that are rendered to the screen
+	std::map<Shader*, std::vector<Entity*>> renderList;
+	// Transparent entities that are rendered to the screen
+	std::map<Shader*, std::vector<Entity*>> transparentRenderList;
+	// Entities that should have an outline
+	std::vector<Entity*> outlineRenderList;
+	// Entities that aren't rendered to the screen but need to be updated
+	std::vector<Entity*> logicEntities;
+	// The list of all meshes in the scene, for drawing geometry in the shadow or SSAO render passes
+	std::vector<MeshComponent*> meshes;
+	std::vector<MeshComponent*> allMeshes;
+
+	void clearCache()
+	{
+		renderList.clear();
+		transparentRenderList.clear();
+		outlineRenderList.clear();
+		logicEntities.clear();
+		meshes.clear();
+		allMeshes.clear();
+	}
+};
+
+class Scene
+{
+public:
+	/// <summary>
+	/// A struct that serves to cache the data obtained when sorting the scene
+	/// </summary>
+	SortedSceneData sortedSceneData;
+
+	/// <summary>
+	/// The current camera used to render the scene
+	/// </summary>
+	CameraComponent* currentCamera;
+
+	CameraComponent* skyCamera;
+
+	DirectionalLightComponent* directionalLight;
+
+	/// <summary>
+	/// Returns a list of raw pointers to the entities in the renderer
+	/// </summary>
+	/// <returns>A vector containing all the raw entities pointers in the renderer</returns>
+	std::vector<Entity*> getEntities();
+
+	/// <summary>
+	/// Adds an entity to the renderer
+	/// </summary>
+	/// <param name="objectPtr">The entity to be added</param>
+	void addEntity(std::unique_ptr<Entity> objectPtr);
+
+	/// <summary>
+	/// Removes an entity from the renderer
+	/// </summary>
+	/// <param name="objectPtr">The entity to be removed</param>
+	/// <returns>True if the entity was successfully removed, false otherwise</return>
+	bool removeEntity(std::unique_ptr<Entity> objectPtr);
+
+	/// <summary>
+	/// Removes an entity from the renderer using a raw pointer
+	/// </summary>
+	/// <param name="rawObjectPtr">The raw pointer to the object</param>
+	/// <returns>True if the entity was successfully removed, false otherwise</returns>
+	bool removeEntity(Entity* rawObjectPtr);
+
+	/// <summary>
+	/// Starts all the entities in the scene
+	/// </summary>
+	void init();
+
+	/// <summary>
+	/// Sorts the scene data and stores it into a struct
+	/// </summary>
+	/// <param name="cameraFrustum">The camera frustum for frustum culling</param>
+	/// <param name="entities">The entities to be recursed upon</param>
+	void getMeshesRecursively(Frustum& cameraFrustum, std::vector<Entity*>& entities);
+
+private:
+	/// <summary>
+	/// The list of entities contained in the scene
+	/// </summary>
+	std::vector<std::unique_ptr<Entity>> entities;
+};
---
+++ src/scene.cpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "scene.hpp"
+
+std::vector<Entity*> Scene::getEntities()
+{
+	std::vector<Entity*> rawPointers;
+
+	for (auto&& entity : this->entities)
+		rawPointers.push_back(entity.get());
+
+	return rawPointers;
+}
+
+void Scene::addEntity(std::unique_ptr<Entity> objectPtr)
+{
+	this->entities.push_back(std::move(objectPtr));
+}
+
+bool Scene::removeEntity(std::unique_ptr<Entity> objectPtr)
+{
+	const int startSize = this->entities.size();
+
+	this->entities.erase(std::remove(this->entities.begin(), this->entities.end(), objectPtr), this->entities.end());
+
+	// Delete was successful if the size of the vector is different
+	return this->entities.size() != startSize;
+}
+
+bool Scene::removeEntity(Entity* rawObjectPtr)
+{
+	for (auto&& entity : this->entities)
+	{
+		if (entity.get() == rawObjectPtr)
+		{
+			this->entities.erase(std::remove(this->entities.begin(), this->entities.end(), entity), this->entities.end());
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void Scene::init()
+{
+	// Start all the entities on the scene
+	for (auto&& entity : this->entities)
+		entity->start();
+}
+
+void Scene::getMeshesRecursively(Frustum& cameraFrustum, std::vector<Entity*>& entities)
+{
+	// TODO : Don't unnecessarily sort the scene every frame if there are no updates in between
+	// We start by sorting the entities depending on if they are renderable objects
+	for (Entity* entity : entities)
+	{
+		if (entity->getIsEnabled())
+		{
+			MeshComponent* mesh = entity->getComponent<MeshComponent>();
+
+			if (mesh == nullptr)
+				this->sortedSceneData.logicEntities.push_back(entity);
+			else // Entities that can be rendered are grouped by shader
+			{
+				this->sortedSceneData.allMeshes.push_back(mesh);
+
+				if (cameraFrustum.isOnFrustum(mesh->getWorldBoundingBox(), mesh->parent->transform))
+				{
+					this->sortedSceneData.meshes.push_back(mesh);
+
+					PBRMaterial* pbrMat = dynamic_cast<PBRMaterial*>(mesh->material.get());
+					if (pbrMat != nullptr && pbrMat->opacity == 1.0f)
+						this->sortedSceneData.renderList[mesh->material->shaderProgram].push_back(entity);
+					else
+						this->sortedSceneData.transparentRenderList[mesh->material->shaderProgram].push_back(entity);
+
+					if (entity->drawOutline)
+						this->sortedSceneData.outlineRenderList.push_back(entity);
+				}
+			}
+
+			this->getMeshesRecursively(cameraFrustum, entity->getChildren());
+		}
+	}
+}
